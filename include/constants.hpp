67a5e4b3d8f2082eff69205ee31959f0c5c90631
Louis Asanaka <25756888+LouisAsanaka@users.noreply.github.com>
2020-05-21 20:36:04 +0800

    fix: tuned the chassis constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -3,9 +3,9 @@
 #include "main.h"
 
 // Robot dimensions
-constexpr QLength WHEEL_TRACK                  = 15_in;
+constexpr QLength WHEEL_TRACK                  = 12.63_in;
 constexpr QLength TRACKING_WHEEL_DIAMETER      = 2.75_in;
-constexpr QLength TRACKING_BACK_WHEEL_DISTANCE = 7.5_in;
+constexpr QLength TRACKING_BACK_WHEEL_DISTANCE = 5.7_in;
 
 // Drive profiling constants
 constexpr double DRIVE_MAX_VEL   = 0.5;
@@ -16,4 +16,4 @@ constexpr double DRIVE_MAX_JERK  = 8.0;
 constexpr double CONTROLLER_DEADBAND = 0.1;
 
 // Component speeds (in percent)
-constexpr double DRIVE_SPEED = 0.8;
+constexpr double DRIVE_SPEED = 0.7;
